3c842cc109e54797a1d275db11dd64c0f7738471
Marc Delagrammatikas <delagrammatikas@gmail.com>
2015-11-22 16:14:44 -0800

    Initial commit

---
+++ TrackMyMpg/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrackMyMpg.MvcApplication" Language="C#" %>
---
+++ TrackMyMpg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrackMyMpg.Startup))]
+namespace TrackMyMpg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
